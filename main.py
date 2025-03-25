cf54422ee033ef80283274d2eae0c7c0a5233980
Shashi sah <sah.shashi2003@gmail.com>
2025-03-25 14:23:10 +0530

    added necessary dependencies

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from coinnect-backend!")
+from fastapi import FastAPI
+from payman.paymanRouter import router as paymanai_router
+import uvicorn
+
+app = FastAPI()
+
+app.include_router(paymanai_router)
+
+@app.get("/")
+async def main():
+    return {"Hello from coinnect-backend!"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="127.0.0.1", port=3000)
